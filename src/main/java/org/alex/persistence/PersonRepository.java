86b7c895816572463967e891ff56126be007e5de
AlexWilliams <alex.williams2000@hotmail.co.uk>
2019-05-23 15:41:34 +0100

    Add Mongo repository

---
+++ src/main/java/org/alex/persistence/PersonRepository.java
@@ -0,0 +1,9 @@
+package org.alex.persistence;
+
+import java.util.Optional;
+
+import org.alex.service.model.Person;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PersonRepository extends MongoRepository<Person, String>{
+}
